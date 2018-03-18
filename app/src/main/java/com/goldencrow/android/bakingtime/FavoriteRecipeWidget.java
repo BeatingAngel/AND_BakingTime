2d0d1f97f5289a3e07d72e4917034582e4bd63a0
Philipp <philipp.hermueller@gmx.at>
2018-03-18 23:19:40 +0100

    Added more, detailed documentation.

--- app/src/main/java/com/goldencrow/android/bakingtime/FavoriteRecipeWidget.java
+++ app/src/main/java/com/goldencrow/android/bakingtime/FavoriteRecipeWidget.java
@@ -22,20 +22,64 @@ import com.goldencrow.android.bakingtime.adapters.RecipeAdapter;
  */
 public class FavoriteRecipeWidget extends AppWidgetProvider {
 
+    /**
+     * This key is used to store a recipe in a Bundle. This bundle will be sent here if the
+     * favorite recipe changed.
+     * <p>
+     * The data set into the bundle in the EntityUtil#setFavoriteRecipeToWidget method.
+     * Those values will be extracted here in the #onReceive method.
+     *
+     * @see #onReceive(Context, Intent)
+     * @see com.goldencrow.android.bakingtime.utils.EntityUtil#setFavoriteRecipeToWidget(Context, String, String)
+     */
     public static final String WIDGET_FAV_KEY ="widget_favorite_recipe_key";
+
+    /**
+     * This key is the access to the name of the favorite recipe that will be shown in the widget.
+     *
+     * @see #onReceive(Context, Intent)
+     * @see com.goldencrow.android.bakingtime.utils.EntityUtil#setFavoriteRecipeToWidget(Context, String, String)
+     */
     public static final String WIDGET_RECIPE_NAME_KEY ="widget_favorite_recipe_name_key";
+
+    /**
+     * This key is the access to the list of ingredients of the favorite recipe that will be shown
+     * in the widget.
+     *
+     * @see #onReceive(Context, Intent)
+     * @see com.goldencrow.android.bakingtime.utils.EntityUtil#setFavoriteRecipeToWidget(Context, String, String)
+     */
     public static final String WIDGET_INGREDIENTS_KEY ="widget_favorite_recipe_ingredients_key";
 
+    /**
+     * It checks if a favorite recipe exists, and if there is one, then it's data will be displayed
+     * in this specific widget from the appWidgetId.
+     * <p>
+     * Additionally, a click on the widget opens the app and lands on the MainActivity.
+     *
+     * @param context           the context from the caller activity.
+     * @param appWidgetManager  the AppWidgetManager from the context.
+     * @param appWidgetId       the ID from a specific widget on the home menu
+     *                          which will get updated.
+     * @param recipeName        the name of the favorite recipe that will be displayed.
+     * @param ingredients       the list of ingredients from the favorite recipe which will
+     *                          be displayed alongside its name.
+     */
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId, String recipeName, String ingredients) {
-        // Construct the RemoteViews object
+        // Construct the RemoteViews object.
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.favorite_recipe_widget);
 
+        // Create the PendingIntent to open the app (start -> MainActivity).
         Intent intent = new Intent(context, MainActivity.class);
         PendingIntent pendingIntent =
                 PendingIntent.getActivity(context, 0, intent, 0);
         views.setOnClickPendingIntent(R.id.widget_layout, pendingIntent);
 
+        // If this method wasn't called because a new recipe was set, then it was called because
+        //   the timer runs out or a new widget was created.
+        // If this is the case, then the favorite recipe information will be set to the widget
+        //   from the stored data in the SharedPreferences.
         String recipeDefaultName = "No Favorite Recipe";
         if (recipeName == null && ingredients == null) {
             SharedPreferences sharedPreferences =
@@ -47,33 +91,56 @@ public class FavoriteRecipeWidget extends AppWidgetProvider {
                     RecipeAdapter.BOOKMARK_INGREDIENTS_KEY, "");
         }
 
+        // Set all data into the appropriate views.
         views.setTextViewText(R.id.widget_recipe_tv, recipeName);
         views.setTextViewText(R.id.widget_ingredients_tv, ingredients);
 
-        // Instruct the widget manager to update the widget
+        // Instruct the widget manager to update the widget.
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
+    /**
+     * If the favorite recipe changed, then this method is triggered. It changes the
+     * favorite recipe in the SharedPreferences for later usage and also updates all the
+     * widgets.
+     *
+     * @param context   the context of the activity which called this method.
+     * @param intent    the Intent that started this onReceive method.
+     *                  It contains (probably) the data of a new favorite recipe.
+     */
     @Override
     public void onReceive(Context context, Intent intent) {
+        // check if this method was launched to change the current favorite recipe.
         if (intent.hasExtra(WIDGET_FAV_KEY)) {
+            // Extract all the information from the Bundle/Intent.
             Bundle bundle = intent.getBundleExtra(WIDGET_FAV_KEY);
             String recipeName = bundle.getString(WIDGET_RECIPE_NAME_KEY);
             String ingredients = bundle.getString(WIDGET_INGREDIENTS_KEY);
 
+            // Get all ID's of the currently existing widgets on the Home Menu.
             AppWidgetManager man = AppWidgetManager.getInstance(context);
             int[] ids = man.getAppWidgetIds(
                     new ComponentName(context, FavoriteRecipeWidget.class));
+
+            // Update all widgets to the new favorite recipe.
             for (int appWidgetId : ids) {
                 updateAppWidget(context,
                         AppWidgetManager.getInstance(context),
                         appWidgetId, recipeName, ingredients);
             }
         } else {
+            // ...otherwise, do what you want.
             super.onReceive(context, intent);
         }
     }
 
+    /**
+     * If the timer runs out, then all the widgets will be updated.
+     *
+     * @param context           the context from the caller activity.
+     * @param appWidgetManager  the AppWidgetManager from the context.
+     * @param appWidgetIds      the ids of all existing widgets on the Home Menu.
+     */
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
@@ -82,11 +149,21 @@ public class FavoriteRecipeWidget extends AppWidgetProvider {
         }
     }
 
+    /**
+     * Not implemented.
+     *
+     * @param context   the context from the caller activity.
+     */
     @Override
     public void onEnabled(Context context) {
         // Enter relevant functionality for when the first widget is created
     }
 
+    /**
+     * Not implemented.
+     *
+     * @param context   the context from the caller activity.
+     */
     @Override
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
--- app/src/main/java/com/goldencrow/android/bakingtime/MainActivity.java
+++ app/src/main/java/com/goldencrow/android/bakingtime/MainActivity.java
@@ -24,7 +24,7 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 /**
- *
+ * The activity which displays all the recipes available.
  *
  * @author Philipp Hermüller
  * @version 2018.3.14
@@ -33,37 +33,61 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity
                         implements RecipeAdapter.RecipeOnClickListener {
 
+    /**
+     * The Debug-Tag used for logging errors, warning, .... in this class.
+     */
     private final String TAG = this.getClass().getSimpleName();
 
+    /**
+     * The Loading Indicator (Donut) which shows that the data is still loading.
+     */
     private ImageView mLoadingIndicatorIv;
 
+    /**
+     * The Adapter for the RecyclerView which handles all the data as well as the action for
+     * each single element/card in it.
+     */
     private RecipeAdapter mAdapter;
+
+    /**
+     * The Grid in which the recipe-cards are located. The column-count will change depending on
+     * the rotation of the device and if it is a tablet or phone.
+     */
     private GridLayoutManager layoutManager;
 
+    /**
+     * Sets up all variables and retrieves the recipes from the web to display them.
+     *
+     * @param savedInstanceState    the saved state before the device rotation.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Initialize the GUI-Elements.
         RecyclerView recipeListRv = findViewById(R.id.recipe_list_rv);
         mLoadingIndicatorIv = findViewById(R.id.loading_indicator_iv);
 
-        // make the donut symbol rotate like a loading indicator
+        // Make the donut symbol rotate like a loading indicator.
         addSpinningEffectToLoader();
 
-        // setting up the RecyclerListView
+        // Setting up the RecyclerListView.
         int gridLayoutDefaultSpanCount = 1;
         layoutManager = new GridLayoutManager(this, gridLayoutDefaultSpanCount);
 
+        // Change the column count of the grid depending on the rotation of the device and if
+        //   it is a phone or tablet.
         changeLayoutSpanCountFitForDevice();
 
+        // Set up the RecyclerView for usage.
         mAdapter = new RecipeAdapter(this, this);
 
         recipeListRv.setLayoutManager(layoutManager);
         recipeListRv.setHasFixedSize(true);
         recipeListRv.setAdapter(mAdapter);
 
-        // getting all the data from the network and set it to the adapter.
+        // Getting all the data from the network and set it to the adapter.
         RecipeEndpointInterface recipeEndpoint =
                 NetworkUtil.getClient().create(RecipeEndpointInterface.class);
 
@@ -71,19 +95,28 @@ public class MainActivity extends AppCompatActivity
         recipeCall.enqueue(new Callback<Recipe[]>() {
             @Override
             public void onResponse(@NonNull Call<Recipe[]> call, @NonNull Response<Recipe[]> response) {
+                // Remove the loading indicator.
                 removeLoadingIndicator();
+                // Get the recipes.
                 Recipe[] recipes = response.body();
+                // Set the recipes to the adapter.
                 mAdapter.setRecipes(recipes);
             }
 
             @Override
             public void onFailure(@NonNull Call<Recipe[]> call, @NonNull Throwable t) {
+                // If it failed, cancel the call and log the message.
                 call.cancel();
                 Log.w(TAG, t.getLocalizedMessage());
             }
         });
     }
 
+    /**
+     * On device rotation, change the column count of the grid.
+     *
+     * @param savedInstanceState    the saved variables and such.
+     */
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
@@ -91,6 +124,11 @@ public class MainActivity extends AppCompatActivity
         changeLayoutSpanCountFitForDevice();
     }
 
+    /**
+     * Changes the column count of the grid depending on the device and rotation.
+     * Phone: 1 col (Land 2 col)
+     * Tablet: 2 col (Land 3 col)
+     */
     private void changeLayoutSpanCountFitForDevice() {
         int smallSpanCount = 1;
         int mediumSpanCount = 2;
@@ -114,23 +152,39 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
+    /**
+     * Adds a rotating animation on the Loading Indicator (Donut), so that the user sees
+     * visual feedback that the app is still running and not frozen.
+     */
     private void addSpinningEffectToLoader() {
+        // A rotation takes 180 degrees.
         RotateAnimation rotate = new RotateAnimation(
                 0, 180,
                 Animation.RELATIVE_TO_SELF,
                 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        // and takes one second.
         rotate.setDuration(1000);
+        // and it does it for infinity.
         rotate.setRepeatCount(Animation.INFINITE);
         rotate.setInterpolator(new LinearInterpolator());
 
         mLoadingIndicatorIv.startAnimation(rotate);
     }
 
+    /**
+     * Removes the Loading Indicator and its animation.
+     * It is called when the data for the recipes is finished loading.
+     */
     private void removeLoadingIndicator() {
         mLoadingIndicatorIv.clearAnimation();
         mLoadingIndicatorIv.setVisibility(View.INVISIBLE);
     }
 
+    /**
+     * If a click on a recipe happened, then open the detail activity.
+     *
+     * @param recipe    contains all the information of the recipe which was clicked upon.
+     */
     @Override
     public void OnClick(Recipe recipe) {
         Intent intent = new Intent(this, RecipeDetailActivity.class);
